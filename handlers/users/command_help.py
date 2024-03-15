493671fb8f65ad9f24bc0718057b14c829e9ad1e
CodWiz <sergeimatrakhib@yandex.ru>
2024-03-15 23:55:52 +0200

    Update command_help.py

--- handlers/users/command_help.py
+++ handlers/users/command_help.py
@@ -4,4 +4,4 @@ from telebot.types import Message
 
 @bot.message_handler(commands=["help"])
 def command_help(message: Message):
-    bot.send_message(message.chat.id, "Qanday yordam kerak?")
+    bot.send_message(message.chat.id, "Тут типа help")
